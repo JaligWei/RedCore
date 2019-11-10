e5d8290bb3f438f103d63c9d15f226cf9ad4229b
JaligWei <gakman@ya.ru>
2019-11-10 22:51:00 +0300

    v.0.0.2

---
+++ core/core.php
@@ -0,0 +1,19 @@
+<?php
+require_once "setting.php";
+require_once $cfg->url_hard."core/plugin/plugin.php";
+
+
+
+//--preinit---//
+require_once $cfg->url_hard."core/plugin/pdo_sql.php";
+
+
+
+//--init-----//
+
+
+
+//--postinit----//
+
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "core/core.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
